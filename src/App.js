3467069e2bc28492c4ebaff178be8832676b6015
joinin20190104 <cym2009@naver.com>
2022-04-12 13:49:14 +0900

    매장 구조도 CSS , 매장 GET 할 때 CSS 수정

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Reservation from "./manage/Reservation";
 import LoginMg from "./manage/LoginMg";
 import SignUpMg from "./manage/SignUpMg";
 import MyCanvas from "./manage/MyCanvas";
+import maincanvas from "./manage/maincanvas";
 
 function App() {
     return (
@@ -35,6 +36,7 @@ function App() {
                     ></Route>
                     <Route path="/register" element={<Register />}></Route>
                     <Route path="/manager/login" element={<LoginMg />}></Route>
+                    <Route path="/ex" element={<maincanvas />}></Route>
                     <Route
                         path="/manager/signup"
                         element={<SignUpMg />}
