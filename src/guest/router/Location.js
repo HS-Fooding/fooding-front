82c3f7666a075692dac275e7d4cc4572e7f8817e
Doyeon04 <rlaehdus0411@naver.com>
2022-05-06 12:25:19 +0900

    지도에 위도,경도별 매장 띄우고 클릭시 매장 정보 get

--- src/guest/router/Location.js
+++ src/guest/router/Location.js
@@ -74,67 +74,117 @@ const HeaderSearch = styled.input`
     outline: none;
   }
 `;
-const clickFunc = (title) => {
-  console.log("title:", title);
+const getClickedMarket = (id) => {
+  console.log("id:", id);
+
+  var config = {
+    method: "get",
+    url: url + `/fooding/restaurant/${id}`,
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
 const Location = () => {
   const [searchedWord, setSearchedWord] = useState();
+
+  const getToken = localStorage.getItem("token");
+
   useEffect(() => {
-    let container = document.getElementById("map");
-    let options = {
-      center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
-      level: 5,
+    let markerdata = [];
+
+    var config = {
+      method: "get",
+      url: url + `/fooding/restaurant?coord=true`,
+      headers: {
+        Authorization: "Bearer " + getToken,
+      },
     };
 
-    const markerdata = [
-      {
-        title: "콜드스퀘어",
-        lat: 37.62197524055062,
-        lng: 127.16017523675508,
-      },
-      {
-        title: "하남돼지집",
-        lat: 37.620842424005616,
-        lng: 127.1583774403176,
-      },
-      {
-        title: "수유리우동",
-        lat: 37.624915253753194,
-        lng: 127.15122688059974,
-      },
-      {
-        title: "맛닭꼬",
-        lat: 37.62456273069659,
-        lng: 127.15211256646381,
-      },
-    ];
+    axios(config)
+      .then(function (response) {
+        //console.log(response.data.content);
 
-    var map = new kakao.maps.Map(container, options);
-    // var markerPosition = new kakao.maps.LatLng(
-    //   37.365264512305174,
-    //   127.10676860117488
-    // );
-    // var marker = new kakao.maps.Marker({
-    //   position: markerPosition,
-    // });
+        response.data.content.map((market) => {
+          const obj = {
+            id: market.id,
+            lat: market.location.coordinate.y, // 위도
+            lng: market.location.coordinate.x, // 경도
+          };
+          markerdata.push(obj);
+        });
 
-    markerdata?.forEach((el) => {
-      // 마커를 생성합니다
-      const marker = new kakao.maps.Marker({
-        //마커가 표시 될 지도
-        map: map,
-        //마커가 표시 될 위치
-        position: new kakao.maps.LatLng(el.lat, el.lng),
-        //마커에 hover시 나타날 title
-        title: el.title,
+        console.log("markderdata:", markerdata);
+
+        return markerdata;
+      })
+      .then((markerdata) => {
+        let container = document.getElementById("map");
+        let options = {
+          center: new kakao.maps.LatLng(37.58265617070882, 127.01017798663574),
+          level: 5,
+        };
+
+        var map = new kakao.maps.Map(container, options);
+        // var markerPosition = new kakao.maps.LatLng(
+        //   37.365264512305174,
+        //   127.10676860117488
+        // );
+        // var marker = new kakao.maps.Marker({
+        //   position: markerPosition,
+        // });
+
+        markerdata.forEach((el) => {
+          // 마커를 생성합니다
+
+          const marker = new kakao.maps.Marker({
+            //마커가 표시 될 지도
+            map: map,
+            //마커가 표시 될 위치
+            position: new kakao.maps.LatLng(el.lat, el.lng),
+            //마커에 hover시 나타날 title
+            id: el.id,
+          });
+
+          kakao.maps.event.addListener(marker, "click", () =>
+            getClickedMarket(el.id)
+          );
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
       });
 
-      kakao.maps.event.addListener(marker, "click", function () {
-        console.log("title:", el.title);
-      });
-    });
     // marker.setMap(map);
   }, []);
+
+  // const markerdata = [
+  //   {
+  //     title: "콜드스퀘어",
+  //     lat: 37.62197524055062,
+  //     lng: 127.16017523675508,
+  //   },
+  //   {
+  //     title: "하남돼지집",
+  //     lat: 37.620842424005616,
+  //     lng: 127.1583774403176,
+  //   },
+  //   {
+  //     title: "수유리우동",
+  //     lat: 37.624915253753194,
+  //     lng: 127.15122688059974,
+  //   },
+  //   {
+  //     title: "맛닭꼬",
+  //     lat: 37.62456273069659,
+  //     lng: 127.15211256646381,
+  //   },
+  // ];
 
   const bringSearchedWord = (e) => {
     setSearchedWord(e.target.value);
