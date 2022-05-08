60ceb182faf2549434c51a68fdf6db3136fadaf0
joinin20190104 <cym2009@naver.com>
2022-05-08 22:41:12 +0900

    url 수정

--- src/guest/router/Login.js
+++ src/guest/router/Login.js
@@ -196,7 +196,7 @@ const Login = () => {
 
     var config = {
       method: "post",
-      url: "http://localhost:8080/fooding/login", //url + 
+      url: url + "/fooding/login",
       headers: {
         //crossDomain: true,
         "Content-Type": "application/json",
