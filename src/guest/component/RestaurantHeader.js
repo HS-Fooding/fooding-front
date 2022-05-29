7fb1d2c3b90bcaebc1e0be33a3dc00367476244c
Doyeon04 <rlaehdus0411@naver.com>
2022-05-29 20:20:25 +0900

    restaurant 헤더 아이콘 변경

--- src/guest/component/RestaurantHeader.js
+++ src/guest/component/RestaurantHeader.js
@@ -2,11 +2,9 @@ import React, { useEffect, useState } from "react";
 import styled, { createGlobalStyle, keyframes } from "styled-components";
 import Header from "../component/Header";
 import { useNavigate, Link } from "react-router-dom";
+import { BiMapAlt } from "react-icons/bi";
+import { FiSearch } from "react-icons/fi";
 
-import { url } from "../../Api";
-// src\Api.js
-//src\guest\component\Login.js
-import { motion, AnimatePresence } from "framer-motion";
 // border: 1px solid black;
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMap, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
@@ -58,17 +56,17 @@ const RestaurantHeader = () => {
             style={{ textDecoration: "none", color: "inherit" }}
             to={`/guest/restaurantSearch`}
           >
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <FiSearch></FiSearch>
           </Link>
         </div>{" "}
-        <p> | </p>{" "}
+        <p style={{ color: "rgba(0,0,0,0.2)" }}> | </p>{" "}
         <div>
           <Link
             style={{ textDecoration: "none", color: "inherit" }}
             to="/guest/location"
             state={{ searchWord: "none" }}
           >
-            <FontAwesomeIcon icon={faMap} />
+            <BiMapAlt></BiMapAlt>
           </Link>
         </div>{" "}
       </MapSearchContainer>
