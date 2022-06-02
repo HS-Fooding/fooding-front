6c51f9ba4e05f41668f954d36ddd424330478be2
joinin20190104 <cym2009@naver.com>
2022-06-03 02:23:23 +0900

    프로필 페이지 유저 정보 추가

--- src/guest/router/MyPageHome.js
+++ src/guest/router/MyPageHome.js
@@ -28,20 +28,62 @@ const Buttons = styled.div`
     margin-left: 10px;
     margin-right: 10px;
 `;
-const Profile = styled.div`
+const ProfileContainer = styled.div`
     width: 100%;
-    height: 100px;
+    height: 300px;
+    margin-top:20px;
+    display:flex;
+    justify-content:center;
+
+`;
+const Profile = styled.div`
+    width:95%;
+    height:300px;
+    display:flex;
+    flex-direction: column;
+
+    .infoContainer{
+        display:flex;
+        
+        height:30px;
+        width:100%;
+        border-bottom:1px solid rgba(0,0,0,0.1);
+        justify-content:space-between;
+        margin-bottom: 30px;
+        .title{
+                color:#FF7B54;
+            }
+        div{
+            margin-right:10px;
+            height:30px;
+            
+        }
+        .favorContainer{
+          
+            height:30px;
+            font-size:11px;
+            display:flex;
+            .info{
+                width:33px;
+                height:10px;
+               
+                
+            }
+            
+        }
+    }
+    
 `;
 const Button = styled.div`
     width: 100%;
     padding: 50px 20px;
-    border-bottom: 1px solid ${(props) => props.theme.borderGrayColor};
+    /* border-bottom: 1px solid ${(props) => props.theme.borderGrayColor}; */
     border-top: 1px solid ${(props) => props.theme.borderGrayColor};
     font-size: 16px;
     display: flex;
     justify-content: space-between;
     box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
-
+    
     div {
         svg {
             color: ${(props) => props.theme.fontGrayColor};
@@ -56,6 +98,7 @@ const Button = styled.div`
 
 const MyPageHome = () => {
     const [myInfo, setMyInfo] = useState();
+    const [changeFavor,setChangeFavor] = useState();
     useEffect(() => {
         const getToken = localStorage.getItem("guestToken");
         const config = {
@@ -69,6 +112,7 @@ const MyPageHome = () => {
             .then((res) => {
                 console.log("data!!", res.data);
                 setMyInfo(res.data);
+                bringFavor(res.data.favor);
             })
             .catch((err) => {
                 console.log(err);
@@ -77,13 +121,65 @@ const MyPageHome = () => {
     const logoutFun = () => {
         localStorage.clear("guestToken");
     };
-
+    const bringFavor = (favorList) =>{
+        let changeFavor = [];
+        let result;
+        favorList.map((one,index)=>{
+            if (one === "KOREAN") result="한식";
+            else if (one === "JAPANESE") result= "일식";
+            else if (one === "CHINESE") result="중식";
+            else if (one === "WESTERN") result="양식";
+            else if (one === "VIETNAM") result="베트남";
+            else if (one === "TAIWAN") result="태국";
+            else if (one === "SNACK") result="분식";
+            else if (one === "NOODLE") result="면요리";
+            else if (one === "BBQ") result="바베큐";
+            else if (one === "PORK") result="돼지고기";
+            else if (one === "BEEF") result="소고기";
+            else if (one === "CHICKEN") result="닭고기";
+            else if (one === "LAMB") result="양고기";
+            else if (one === "CAFE") result="카페";
+            else if (one === "DESSERT") result="디저트";
+            else if (one === "BAR") result="바";
+            else if (one === "PUB") result="술집";
+            changeFavor.push(result);
+        })
+        console.log("changeFavor",changeFavor);
+        setChangeFavor(changeFavor);
+    }
     return (
         <Container>
             <Header back="/guest/restaurantList" title={"마이 페이지"} />
             <Buttons>
-                <Profile>프로필</Profile>
+                <ProfileContainer>
+                    <Profile>
+                    <div className="infoContainer" style={{borderBottom:"solid 3px #FF7B54"}}>
+                            <div style={{color:"#FF7B54",fontWeight:"bold"}}>개인정보 </div>
+                        </div>
+                        
+                        <div className="infoContainer">
+                            <div className="title">닉네임 </div>
+                            <div>{myInfo?.nickName}</div>
+                        </div>
+                        <div className="infoContainer">
+                            <div className="title">이름 </div>
+                            <div>{myInfo?.name}</div>
+                        </div>
+                        <div className="infoContainer">
+                            <div className="title">취향 </div>
+                            <div className="favorContainer">
+                            {changeFavor?.map((info,index)=>{
+                                return (<div className="info">{info}</div>)
+                            })}</div>
+                        </div>
+                        <div className="infoContainer">
+                            <div className="title">성별 </div>
+                            <div>{myInfo?.sex ? "남자" : "여자"}</div>
+                        </div>
+                    </Profile>                                                                                        
+                </ProfileContainer>
                 {/* TODO : 프로필 정보 뿌리기 (myInfo) */}
+
                 <Link style={{ textDecoration: "none", color: "inherit" }} to={"/guest/login"}>
                     <Button onClick={logoutFun}>로그아웃</Button>
                 </Link>
